8276291cedf99eab6129c25901631fda9a554d94
y52en <61645319+y52en@users.noreply.github.com>
2021-05-28 08:25:53 +0900

    Update rankEventBounus.js

--- rankEventBounus.js
+++ rankEventBounus.js
@@ -12,3 +12,28 @@ arr.sort((a,b)=>{
     if(a[1] < b[1]) return 1;
     return 0;
 })
+
+
+c_fn = (x) => "星" + (x.m_Rare + 1) + "_" + x.m_Name + "_" + x.year
+c_ls = {}
+vue.$db.CharacterListArray
+.filter(x => !Boolean((x.m_CharaID) % 2) )
+.filter(x =>  x.m_Rare === 4)
+.filter(x => x.year === 2017 || x.year === 2018)
+.forEach(x => {c_ls[c_fn(x)] = 0})
+
+b_ls = vue.$db.EventQuestDropExt
+.filter(x => !Boolean((x.m_CharaID) % 2) )
+.map(x => x.m_CharaID)
+.map(x => vue.$db.CharacterList[x])
+.filter(x =>  x.m_Rare === 4)
+.filter(x => x.year === 2017 || x.year === 2018)
+.map(x => c_fn(x))
+.forEach(x => c_ls[x] += 1)
+
+arr = Object.entries(c_ls)
+arr.sort((a,b)=>{
+    if(a[1] > b[1]) return -1;
+    if(a[1] < b[1]) return 1;
+    return 0;
+}).reverse()
