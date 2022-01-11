e676e4c76a37e8b623025a0d143ae7f54517efc9
y52en <61645319+y52en@users.noreply.github.com>
2022-01-11 22:11:21 +0900

    Update mirr.js

--- mirr.js
+++ mirr.js
@@ -1,20 +1,30 @@
-/*! js-cookie v3.0.1 | MIT */
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self,function(){var n=e.Cookies,o=e.Cookies=t();o.noConflict=function(){return e.Cookies=n,o}}())}(this,(function(){"use strict";function e(e){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var o in n)e[o]=n[o]}return e}return function t(n,o){function r(t,r,i){if("undefined"!=typeof document){"number"==typeof(i=e({},o,i)).expires&&(i.expires=new Date(Date.now()+864e5*i.expires)),i.expires&&(i.expires=i.expires.toUTCString()),t=encodeURIComponent(t).replace(/%(2[346B]|5E|60|7C)/g,decodeURIComponent).replace(/[()]/g,escape);var c="";for(var u in i)i[u]&&(c+="; "+u,!0!==i[u]&&(c+="="+i[u].split(";")[0]));return document.cookie=t+"="+n.write(r,t)+c}}return Object.create({set:r,get:function(e){if("undefined"!=typeof document&&(!arguments.length||e)){for(var t=document.cookie?document.cookie.split("; "):[],o={},r=0;r<t.length;r++){var i=t[r].split("="),c=i.slice(1).join("=");try{var u=decodeURIComponent(i[0]);if(o[u]=n.read(c,u),e===u)break}catch(e){}}return e?o[e]:o}},remove:function(t,n){r(t,"",e({},n,{expires:-1}))},withAttributes:function(n){return t(this.converter,e({},this.attributes,n))},withConverter:function(n){return t(e({},this.converter,n),this.attributes)}},{attributes:{value:Object.freeze(o)},converter:{value:Object.freeze(n)}})}({read:function(e){return'"'===e[0]&&(e=e.slice(1,-1)),e.replace(/(%[\dA-F]{2})+/gi,decodeURIComponent)},write:function(e){return encodeURIComponent(e).replace(/%(2[346BF]|3[AC-F]|40|5[BDE]|60|7[BCD])/g,decodeURIComponent)}},{path:"/"})}));
-
 window.onload = () => {
   const user_agent = navigator.userAgent;
 
   if (!user_agent.match("MR_APP/")) {
-    throw alert("Please change your user agent to MR_APP~");
+    throw alert("Please change your user agent to MR_APP");
   }
+  const mr_id = document.cookie
+    .split(";")
+    .find((item) => item.trim().startsWith("mr_id="))
+    .trim()
+    .replace("mr_id=", "");
 
+  function via_error() {
+    alert(
+      "おそらくviaのバグでCookieの上書きができません。(Chromeなどではうまくいく)\n設定->一般->プライベートモードの消去->CookieでCookieを消去してください。"
+    );
+  }
   const account_name = Mirrativ.currentUser.name;
-    if (!account_name) {
-      Cookies.remove("mr_id");
-      Cookies.remove("f");
+  if (!account_name) {
+    if (mr_id) {
+      via_error();
+      return;
+    }
+
     alert("you need to login first");
-      const id = prompt("please enter mr_id");
-      Cookies.set("mr_id", id);
+    const id = prompt("please enter mr_id");
+    document.cookie = `mr_id=${id}; path=/;`;
     location.reload();
   }
 
@@ -38,11 +48,13 @@ window.onload = () => {
     return blob;
   }
 
-    function logout() {
-        Cookies.remove("mr_id");
-        Cookies.remove("f");
-        location.reload();
-    }
+  function logout() {
+    via_error();
+    return;
+    // Cookies.remove("mr_id");
+    // Cookies.remove("f");
+    // location.reload();
+  }
 
   let img_file;
   async function exec() {
