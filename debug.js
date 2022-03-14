3d071d33ec09c0d362398d6212c75b2f891549ac
y52en <61645319+y52en@users.noreply.github.com>
2022-03-15 05:17:39 +0900

    Create debug.js

---
+++ debug.js
@@ -0,0 +1,4 @@
+// codegolf?
+// base: https://gist.github.com/suderman/aa2338c5b1f11691c64aacae65b6ce4f
+
+javascript:((e,n=d=%3Ee.body.appendChild(d),a=d=%3Ee.createElement('script'),i=a(),r=a(),m=a(),o=a(),t=(d,e)=%3Ed.src='//cdn.jsdelivr.net/npm/eruda'+e+'/eruda'+e+'.min.js')=%3E%7Bt(i,''),t(m,'-timing'),t(o,'-code'),t(r,'-dom'),n(i),i.onload=(e=%3E%7Blet%20a=eruda,i=d=%3Ea.add(d);a.init(),n(m),m.onload=d=%3Ei(erudaTiming),n(o),o.onload=d=%3Ei(erudaCode),n(r),r.onload=d=%3Ei(erudaDom)%7D)%7D)(document)
